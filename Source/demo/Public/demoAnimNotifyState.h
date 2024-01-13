28691466c40aaa4ff7b561367589a6623df67b9f
PAVEL KOSHKIN <76266087+KoshkinPV@users.noreply.github.com>
2024-01-13 17:40:17 +0300

    Add basic attack and dummy enemy

---
+++ Source/demo/Public/demoAnimNotifyState.h
@@ -0,0 +1,17 @@
+// Fill out your copyright notice in the Description page of Project Settings.
+
+#pragma once
+
+#include "CoreMinimal.h"
+#include "Animation/AnimNotifies/AnimNotifyState.h"
+#include "demoAnimNotifyState.generated.h"
+
+/**
+ * 
+ */
+UCLASS()
+class DEMO_API UdemoAnimNotifyState : public UAnimNotifyState
+{
+	GENERATED_BODY()
+	
+};
